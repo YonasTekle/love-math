02d157414f3b50d3f6205d9e038df0e7ce45063b
Yonas Tekle <yonas.t.tekle@outlook.com>
2021-10-08 19:15:56 +0000

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calcuateCorrectAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion()
